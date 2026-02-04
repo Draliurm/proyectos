de74ea4be1fd407accdea36e364014c5e2a62f88
Draliurm <Draliurm@gmail.com>
2026-02-04 15:40:30 -0600

    Feal: Password access loop

---
+++ bucles/bucleContraseña.py
@@ -0,0 +1,9 @@
+clave= "Papagayo14801612"
+for i in range(3):
+    claveInput= input("Ingrese su contraseña: ")
+    if claveInput == clave:
+        print("Acceso concedido")
+        break
+    else:
+        print("Acceso denegado")
+    
