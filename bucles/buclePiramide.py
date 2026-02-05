2cfdbaef3a3637469754c631d67e65a7b58beb41
Draliurm <Draliurm@gmail.com>
2026-02-05 13:16:32 -0600

    Feal: Half rhombus activity with for

---
+++ bucles/buclePiramide.py
@@ -0,0 +1,9 @@
+tri= int(input("Ingrese el valor deseado: "))
+for i in (range(0,tri+1,1)) :
+    for j in range (-0-i,0,1):
+            print((j), end=" ")
+    print()
+for i in reversed(range(0,tri,1)):
+    for j in range (-0-i,0,1):
+            print((j), end=" ")
+    print()
