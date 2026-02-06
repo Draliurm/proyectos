28923d18d3c33bf5d4a5dc392f0e812749e905eb
Draliurm <Draliurm@gmail.com>
2026-02-06 11:20:07 -0600

    Feat: Arithmetic operations activity

---
+++ repaso/operacionesAritmeticas.py
@@ -0,0 +1,17 @@
+n1= float(input("Ingrese el primer numero: "))
+n2= float(input("Ingrese el segundo numero: "))
+suma= n1+n2
+resta= n1-n2
+multiplicacion= n1*n2
+division= n1/n2
+division2= n1//n2
+modulo= n1%n2
+potencia= n1**n2
+
+print(f"la suma es: {suma}")
+print(f"la resta es: {resta}")
+print(f"la multiplicacion es: {multiplicacion}")
+print(f"la division es: {round(division,2)}")
+print(f"la division2 es: {division2}")
+print(f"la modulo es: {modulo}")
+print(f"la potencia es: {potencia}")
