bb81a768d01ee61baa1e0ff886e06bb78d0c2f0e
Draliurm <Draliurm@gmail.com>
2026-02-06 11:45:04 -0600

    Feal: Calculator activity

--- condicionales/calculadora.py
+++ condicionales/calculadora.py
@@ -1,24 +1,39 @@
-try:
-    n1 = float(input("Ingrese el primer numero: "))
-    n2 = float(input("Ingrese el segundo numero: "))
-    operacion = input("Ingrese el operador: ")
+valor = ""
+while valor !='salir':
+    try:
+        n1 = float(input("Ingrese el primer numero: "))
+        n2 = float(input("Ingrese el segundo numero: "))
+        operacion = input("Ingrese el operador: ")
 
-    match operacion:
-        case "+":
-            print("suma")
-            print(n1+n2)
-        case "-":
-            print("resta")
-            print(n1-n2)
-        case "*":
-            print("multiplicacion")
-            print(n1*n2)
-        case "/":
-            if n2 == 0:
-                print("Error")
-            else:
+        match operacion:
+            case "+":
+                print("suma")
+                print(n1+n2)
+            case "-":
+                print("resta")
+                print(n1-n2)
+            case "*":
+                print("multiplicacion")
+                print(n1*n2)
+            case "**":
+                print("potencia")
+                print(n1**n2)
+            case "//":
+                print("division2")
+                print(n1//n2)
+            case "%":
+                print("modulo")
+                print(n1%n2)
+            case "/":
+                if n2 == 0:
+                    print("Error")
+                else:
+                    print("division")
+                    print(n1/n2)
+            case _:
+                print("Salir")
 
-                print("division")
-                print(n1/n2)
-except ValueError:
-    print("Ingrese un numero valido")
+        valor= input("Ingrese la accion a realizar: ")
+
+    except ValueError:
+        print("Ingrese un numero valido")
